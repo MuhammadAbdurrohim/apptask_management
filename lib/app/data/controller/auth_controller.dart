209d377876b27e8063fe924fbd156a213425cfb2
Muhammad Abdurrohim <muhammadabdurrohim574@gmail.com>
2022-07-31 16:13:20 +0700

    commit 16 sarch data from firestore database02

--- lib/app/data/controller/auth_controller.dart
+++ lib/app/data/controller/auth_controller.dart
@@ -60,12 +60,12 @@ print(googleUser!.email);
       'createdAt': _userCredential!.user!.metadata.creationTime.toString(),
       'lastLoginAt': _userCredential!.user!.metadata.lastSignInTime.toString(),
      // 'list_cari': [R,RO,ROH,ROHIM]
-  }).then((value) {
+  }).then((value) async {
     String temp = '';
     try {
       for (var i = 0; i < googleUser.displayName!.length; i++) {
         temp = temp + googleUser.displayName! [i];
-        users.doc(googleUser.email).set({
+      await  users.doc(googleUser.email).set({
           'list_cari': FieldValue.arrayUnion([temp.toUpperCase()])
         }, SetOptions(merge: true));
       }
@@ -74,7 +74,7 @@ print(googleUser!.email);
     }
   });
   } else {
-    users.doc(googleUser.email).set({
+    users.doc(googleUser.email).update({
       'lasLoginAt': _userCredential!.user!.metadata.lastSignInTime.toString(),
     });
     
@@ -107,6 +107,7 @@ var hasilPencarian = [].obs as hasil;
       }
 
     if (kataCari.isNotEmpty) {
+      hasilPencarian.value = [];
       kataCari.forEach((element) {
         print(element);
       hasilPencarian.add(element);
